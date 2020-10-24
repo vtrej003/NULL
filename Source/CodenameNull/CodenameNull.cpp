58a50a1f0e7db62a86ddc2f133277cab2259b349
theclone <mrperson7895@gmail.com>
2020-10-23 17:34:54 -0700

    Initial commit

---
+++ Source/CodenameNull/CodenameNull.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CodenameNull.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CodenameNull, "CodenameNull" );
+ 
